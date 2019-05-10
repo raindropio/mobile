133e3aad6bc1055b758871b3639369fc8d6a8213
Rustem Mussabekov <rustemmussabekov@Macintosh.local>
2019-05-10 18:01:41 +0300

    Fix Safari preview UTF8 urls on iOS

--- ios/Modules/SafariBridge.m
+++ ios/Modules/SafariBridge.m
@@ -13,54 +13,54 @@ RCT_EXPORT_MODULE();
 }
 
 RCT_EXPORT_METHOD(open:(NSString *)componentId options:(NSDictionary *)options) {
-  
-  NSString* url = [options valueForKey:@"url"];
-  NSNumber* readerMode = [options valueForKey:@"readerMode"];
-  NSNumber* reactTag = [options valueForKey:@"reactTag"];
-  NSNumber* preferredBarTintColor = [options valueForKey:@"preferredBarTintColor"];
-  NSNumber* preferredControlTintColor = [options valueForKey:@"preferredControlTintColor"];
-  
-  UIViewController *vc = [ReactNativeNavigation findViewController:componentId];
-  
-  SFSafariViewController *safariViewController = [[SFSafariViewController alloc] initWithURL:[[NSURL alloc] initWithString:url] entersReaderIfAvailable:[readerMode boolValue]];
-  
-  if (preferredBarTintColor) {
-    safariViewController.preferredBarTintColor = [RCTConvert UIColor:preferredBarTintColor];
-  }
-  
-  if (preferredControlTintColor) {
-    safariViewController.preferredControlTintColor = [RCTConvert UIColor:preferredControlTintColor];
-  }
-  
-  if (@available(iOS 11.0, *)) {
-    safariViewController.dismissButtonStyle = SFSafariViewControllerDismissButtonStyleDone;
-  }
-  
-  (void)safariViewController.view;
-  
-  if ([reactTag intValue] >= 0) {
-    if ([vc isKindOfClass:[RNNRootViewController class]]) {
-      RNNRootViewController* rootVc = (RNNRootViewController*)vc;
-      rootVc.previewController = safariViewController;
-      rootVc.previewCallback = ^(UIViewController *vc) {
-        RNNRootViewController* theVc = (RNNRootViewController*)vc;
-        [vc.navigationController presentViewController:safariViewController animated:NO completion:nil];
-        [theVc.eventEmitter sendComponentDidAppear:theVc.layoutInfo.componentId componentName:@"SAFARI_VIEW"];
-      };
-      RCTExecuteOnMainQueue(^{
-        UIView *view = [[ReactNativeNavigation getBridge].uiManager viewForReactTag:reactTag];
-        [rootVc registerForPreviewingWithDelegate:(id)rootVc sourceView:view];
-      });
+  RCTExecuteOnMainQueue(^{
+    NSURL* url = [[NSURL alloc] initWithString:[[options valueForKey:@"url"] stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLFragmentAllowedCharacterSet]]];
+    NSNumber* readerMode = [options valueForKey:@"readerMode"];
+    NSNumber* reactTag = [options valueForKey:@"reactTag"];
+    NSNumber* preferredBarTintColor = [options valueForKey:@"preferredBarTintColor"];
+    NSNumber* preferredControlTintColor = [options valueForKey:@"preferredControlTintColor"];
+    
+    UIViewController *vc = [ReactNativeNavigation findViewController:componentId];
+    
+    SFSafariViewController *safariViewController = [[SFSafariViewController alloc] initWithURL:url entersReaderIfAvailable:[readerMode boolValue]];
+    
+    if (preferredBarTintColor) {
+      safariViewController.preferredBarTintColor = [RCTConvert UIColor:preferredBarTintColor];
     }
-  } else {
-    dispatch_async( dispatch_get_main_queue(), ^{
+    
+    if (preferredControlTintColor) {
+      safariViewController.preferredControlTintColor = [RCTConvert UIColor:preferredControlTintColor];
+    }
+    
+    if (@available(iOS 11.0, *)) {
+      safariViewController.dismissButtonStyle = SFSafariViewControllerDismissButtonStyleDone;
+    }
+    
+    (void)safariViewController.view;
+    
+    if ([reactTag intValue] >= 0) {
+      if ([vc isKindOfClass:[RNNRootViewController class]]) {
+        
+          RNNRootViewController* rootVc = (RNNRootViewController*)vc;
+          rootVc.previewController = safariViewController;
+          rootVc.previewCallback = ^(UIViewController *vc) {
+            RNNRootViewController* theVc = (RNNRootViewController*)vc;
+            [vc.navigationController presentViewController:safariViewController animated:NO completion:nil];
+            [theVc.eventEmitter sendComponentDidAppear:theVc.layoutInfo.componentId componentName:@"SAFARI_VIEW"];
+          };
+        
+          UIView *view = [[ReactNativeNavigation getBridge].uiManager viewForReactTag:reactTag];
+          [rootVc registerForPreviewingWithDelegate:(id)rootVc sourceView:view];
+        
+      }
+    } else {
       [vc.navigationController presentViewController:safariViewController animated:YES completion:nil];
       if ([vc isKindOfClass:[RNNRootViewController class]]) {
         RNNRootViewController* rootVc = (RNNRootViewController*)vc;
         [rootVc.eventEmitter sendComponentDidAppear:rootVc.layoutInfo.componentId componentName:@"SAFARI_VIEW"];
       }
-    });
-  }
+    }
+  });
 }
 
 @end
