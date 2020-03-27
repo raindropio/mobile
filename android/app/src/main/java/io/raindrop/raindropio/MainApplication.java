e49c639091433ffe7de51967cf8df6a680325c8e
Exentrich <exentrich@MacBook-Pro-Exentrich.local>
2020-03-27 19:24:03 +0300

    Upgrade to RN 62

--- android/app/src/main/java/io/raindrop/raindropio/MainApplication.java
+++ android/app/src/main/java/io/raindrop/raindropio/MainApplication.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import androidx.multidex.MultiDex;
 
 import com.facebook.react.PackageList;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import io.raindrop.raindropio.Extension.ExtensionPackage;
@@ -51,5 +54,42 @@ public class MainApplication extends NavigationApplication {
     @Override
     public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    }
+
+    /**
+        * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+        * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+        *
+        * @param context
+        * @param reactInstanceManager
+    */
+    private static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+        if (BuildConfig.DEBUG) {
+            try {
+                /*
+                We use reflection here to pick up the class that initializes Flipper,
+                since Flipper library is not available in release mode
+                */
+                Class<?> aClass = Class.forName("io.raindrop.raindropio.ReactNativeFlipper");
+                aClass
+                    .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+                    .invoke(null, context, reactInstanceManager);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
