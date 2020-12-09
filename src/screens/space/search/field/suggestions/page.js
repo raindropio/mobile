f05972ba98a06610aa6abb141f8101746d3836cd
Rustem Mussabekov <winlinx@ya.ru>
2020-12-09 16:37:58 +0300

    Limit tags count by default in search screen

--- src/screens/space/search/field/suggestions/page.js
+++ src/screens/space/search/field/suggestions/page.js
@@ -1,21 +1,42 @@
 import React from 'react'
+import t from 't'
 
 import { Wrap, Scroll, Items } from './page.style'
 import SectionTags from 'co/tags/section'
 import SectionFilters from 'co/filters/section'
+import { Button, Label, IconWrap } from './item.style'
+import Icon from 'co/icon'
 
 class SearchSuggestionsPage extends React.Component {
+    state = {
+        compact: true
+    }
+
+    onShowAllPress = ()=>
+        this.setState({ compact: false })
+
     renderItem = (item)=>
         this.props.renderItem({ item })
 
     render() {
         const { tags, filters } = this.props
+        const { compact } = this.state
 
         return (
             <Wrap>
                 <Scroll>
                     <SectionTags />
-                    <Items>{tags.map(this.renderItem)}</Items>
+                    <Items>
+                        {(compact ? tags.slice(0, 30) : tags).map(this.renderItem)}
+                        {compact && (
+                            <Button onPress={this.onShowAllPress}>
+                                <IconWrap>
+                                    <Icon name='arrow-down-s' />
+                                </IconWrap>
+                                <Label>{t.s('showAll')}</Label>
+                            </Button>
+                        )}
+                    </Items>
                     
                     <SectionFilters />
                     <Items>{filters.map(this.renderItem)}</Items>
