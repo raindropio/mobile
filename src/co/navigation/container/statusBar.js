0cccc045c797a0cd1b0531a3263fee14b1c70b1b
Rustem Mussabekov <winlinx@ya.ru>
2020-12-13 18:05:47 +0300

    Fix jump on app open in Android

--- src/co/navigation/container/statusBar.js
+++ src/co/navigation/container/statusBar.js
@@ -4,7 +4,7 @@ import { Platform, StatusBar } from 'react-native'
 export default Platform.OS=='android' ? 
     styled(StatusBar).attrs(({theme})=>({
         animated: false,
-        translucent: true,
+        translucent: theme.isExtension, //main app should be false, otherwise it jumps right after open
         backgroundColor: theme.background.regular, //transparent doesnt work in android <=25
         barStyle: theme.dark ? 'light-content' : 'dark-content'
     }))`` :
