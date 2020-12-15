9d2646f7d1197ab0b2f37b887351ddb1444e3946
Rustem Mussabekov <winlinx@ya.ru>
2020-12-15 14:31:32 +0300

    Fix search suggestions sticky headers

--- src/screens/space/search/field/suggestions/page.js
+++ src/screens/space/search/field/suggestions/page.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useMemo, useCallback } from 'react'
 import t from 't'
 
 import { Wrap, Scroll, Items } from './page.style'
@@ -7,51 +7,53 @@ import SectionFilters from 'co/filters/section'
 import { Button, Label, IconWrap } from './item.style'
 import Icon from 'co/icon'
 
-class SearchSuggestionsPage extends React.Component {
-    state = {
-        compact: true
-    }
+export default function SearchSuggestionsPage({ tags, filters, renderItem }) {
+    //compact
+    const [compact, setCompact] = useState(true)
+    const onShowAllPress = useCallback(() => {
+        setCompact(false)
+    }, [])
 
-    stickyHeaderIndices = [0, 2]
+    const reduced = useMemo(()=>
+        (compact && tags.length > 30)
+    , [tags.length, compact])
 
-    onShowAllPress = ()=>
-        this.setState({ compact: false })
+    //sticky
+    const stickyHeaderIndices = useMemo(()=>[
+        ...tags.length ? [0] : [],
+        ...filters.length ? [tags.length ? 2 : 0] :[]
+    ], [tags, filters])
 
-    renderItem = (item)=>
-        this.props.renderItem({ item })
+    //item
+    const reRenderItem = useCallback(item=>
+        renderItem({item}),
+        [renderItem]
+    )
 
-    render() {
-        const { tags, filters } = this.props
-        const { compact } = this.state
-        const reduced = (compact && tags.length > 30)
-
-        return (
-            <Wrap>
-                <Scroll stickyHeaderIndices={this.stickyHeaderIndices}>
-                    {!!tags.length && (<>
-                        <SectionTags hidden={false} />
-                        <Items>
-                            {(reduced ? tags.slice(0, 30) : tags).map(this.renderItem)}
-                            
-                            {!!reduced && (
-                                <Button onPress={this.onShowAllPress}>
-                                    <IconWrap>
-                                        <Icon name='arrow-down-s' />
-                                    </IconWrap>
-                                    <Label>{t.s('showAll')}</Label>
-                                </Button>
-                            )}
-                        </Items>
-                    </>)}
-                    
-                    {!!filters.length && (<>
-                        <SectionFilters hidden={false} />
-                        <Items>{filters.map(this.renderItem)}</Items>
-                    </>)}
-                </Scroll>
-            </Wrap>
-        )
-    }
+    return (
+        <Wrap>
+            <Scroll stickyHeaderIndices={stickyHeaderIndices}>
+                {!!tags.length && (<SectionTags hidden={false} />)}
+                {!!tags.length && (
+                    <Items>
+                        {(reduced ? tags.slice(0, 30) : tags).map(reRenderItem)}
+                        
+                        {!!reduced && (
+                            <Button onPress={onShowAllPress}>
+                                <IconWrap>
+                                    <Icon name='arrow-down-s' />
+                                </IconWrap>
+                                <Label>{t.s('showAll')}</Label>
+                            </Button>
+                        )}
+                    </Items>
+                )}
+                
+                {!!filters.length && (<SectionFilters hidden={false} />)}
+                {!!filters.length && (
+                    <Items>{filters.map(reRenderItem)}</Items>
+                )}
+            </Scroll>
+        </Wrap>
+    )
 }
-
-export default SearchSuggestionsPage
