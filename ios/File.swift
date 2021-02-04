0f5d750e0cc5d1862a4c80c9e95782bdd7f69f99
Rustem Mussabekov <winlinx@ya.ru>
2021-02-04 13:28:23 +0300

    - Remove unused Swift standard libraries in iOS - Compress images

--- ios/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  rn3
-//
-//  Created by Rustem Mussabekov on 01.10.2020.
-//
-
-import Foundation
--- ios/extension/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  extension
-//
-//  Created by Rustem Mussabekov on 01.10.2020.
-//
-
-import Foundation
