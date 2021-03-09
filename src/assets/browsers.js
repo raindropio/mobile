a0ff7f4ff2b2ec496ba12f932a596403571d1edc
Rustem Mussabekov <winlinx@ya.ru>
2021-03-09 16:55:47 +0300

    - Remove old opera browser from ios - Fix data - Upgrade deps

--- src/assets/browsers.js
+++ src/assets/browsers.js
@@ -8,7 +8,7 @@ export default Platform.select({
         {id: 'ios.chrome', label: 'Chrome',                 prefix: 'googlechrome://', icon: 'chrome'},
         {id: 'ios.firefox', label: 'Firefox',               prefix: 'firefox://open-url?url=https://', icon: 'firefox'},
         {id: 'ios.firefox-focus', label: 'Firefox Focus',   prefix: 'firefox-focus://open-url?url=https://', icon: 'firefox'},
-        {id: 'ios.opera', label: 'Opera',                   prefix: 'opera://open-url?url=https://', icon: 'opera'},
+        //{id: 'ios.opera', label: 'Opera',                   prefix: 'opera://open-url?url=https://', icon: 'opera'},
         {id: 'ios.opera-touch', label: 'Opera Touch',       prefix: 'touch-https://', icon: 'opera'},
         {id: 'ios.edge', label: 'Edge',                     prefix: 'microsoft-edge-https://', icon: 'edge'},
         {id: 'ios.dolphin', label: 'Dolphin',               prefix: 'dolphin://'},
