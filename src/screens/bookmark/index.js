66ca11611ed86d64f36cf58f9f68838893baef97
Rustem Mussabekov <winlinx@ya.ru>
2021-04-14 16:09:42 +0300

    More reliable way to exclude pods from share ios

--- src/screens/bookmark/index.js
+++ src/screens/bookmark/index.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import Stack from 'co/navigation/stack'
 import Tint from 'co/bookmarks/item/tint'
+import { useTheme } from 'styled-components'
 
 import Edit from './edit'
-import Add from './add'
 import Cover from './cover'
 import Tags from './tags'
 import Path from './path'
 
 function Bookmark({ route: { params={} } }) {
+    const { isExtension } = useTheme()
+
     return (
         <Tint _id={params.params ? params.params._id : params._id}>
             <Stack.Navigator>
                 <Stack.Screen name='edit' component={Edit} options={Edit.options} initialParams={params} />
-                <Stack.Screen name='add' component={Add} options={Add.options} />
+                {!isExtension && <Stack.Screen name='add' component={require('./add').default} options={require('./add').options} />}
                 <Stack.Screen name='cover' component={Cover} options={Cover.options} />
                 <Stack.Screen name='tags' component={Tags} options={Tags.options} />
                 <Stack.Screen name='path' component={Path} options={Path.options} />
