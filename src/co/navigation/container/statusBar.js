67b4db265f0b516ef1fb3543e15b00b750b0d545
Rustem Mussabekov <winlinx@ya.ru>
2021-10-10 17:33:53 +0300

    Fix statusbar for old android versions

--- src/co/navigation/container/statusBar.js
+++ src/co/navigation/container/statusBar.js
@@ -5,7 +5,7 @@ export default Platform.OS=='android' ?
     styled(StatusBar).attrs(({theme})=>({
         animated: false,
         translucent: theme.isExtension, //main app should be false, otherwise it jumps right after open
-        backgroundColor: theme.background.regular, //transparent doesnt work in android <=25
+        backgroundColor: !theme.dark && Platform.Version < 23 ? '#00000060' : theme.background.regular,
         barStyle: theme.dark ? 'light-content' : 'dark-content'
     }))`` :
     StatusBar
