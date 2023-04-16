05aba447e2c5f86464fb7da7e962ebd0caa75478
Rustem Mussabekov <winlinx@ya.ru>
2023-04-16 15:07:12 +0300

    Ask for Notifications permission

--- android/app/src/main/java/io/raindrop/raindropio/Pushes/Pushes.java
+++ android/app/src/main/java/io/raindrop/raindropio/Pushes/Pushes.java
@@ -1,12 +1,18 @@
 package io.raindrop.raindropio.Pushes;
 
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -43,6 +49,12 @@ public class Pushes extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void setUserId(String userId, String token) {
+        if (Build.VERSION.SDK_INT >= 33) {
+            if (ContextCompat.checkSelfPermission(context, POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(getCurrentActivity(), new String[]{POST_NOTIFICATIONS}, 1);
+            }
+        }
+
         PushNotifications.setUserId(userId, new DumbTokenProvider(token));
         Log.d("pushNotifications", "Set userId: "+userId+" with token: "+token);
     }
@@ -71,6 +83,8 @@ public class Pushes extends ReactContextBaseJavaModule {
     static public void onIntent(Intent intent, @NonNull final Activity activity) {
         Bundle extras = intent.getExtras();
         if (extras == null) return;
+
+        Log.d("pushNotifications", "Received");
 
         String raindrop = extras.getString("raindrop");
         if (raindrop == null) return;
